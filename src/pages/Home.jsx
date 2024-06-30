75aca4eb0c8166d0dcab66fe328afed0f2f3ac06
Matic.dev <olaitantoyeeb1@gmail.com>
2024-06-30 20:25:36 +0100

    cross checking and cleaning up the code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import me from '../assets/Images/HomePic.png';
 const Home = () => {
   return (
     <div className=''>
-    <div className='container bg-[#F5FCFF] mx-auto flex flex-col-reverse place-content-center xl:flex-row px-4 xl:px-16 pt-[80px] md:pt-0 md:space-y-16 xl:space-y-0 xl:pt-[120px] py-12' id='home'>
+    <div className='container bg-[#F5FCFF] mx-auto flex flex-col-reverse place-content-center xl:flex-row px-4 xl:px-16 pt-[80px] md:space-y-16 xl:space-y-0 xl:pt-[120px] py-12' id='home'>
       <div className="flex flex-col justify-center items-center xl:items-start xl:w-1/2 space-y-2 xl:space-y-4 text-center xl:text-left mt-8 md:mt-0 md:space-y-8 xl:mt-0 ">
         <h3 className='text-lg font-[600]'>Hey, I am John</h3>
         <h1 className='text-3xl xl:text-[55px] font-[700] leading-tight'>I develop dynamic and User-Centered Web Applications</h1>
